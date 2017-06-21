dbce2c2e326b4f22822f6742572496bcc01a5bb8
Ed Siok <sioked@gmail.com>
2017-06-21 13:53:26 -0500

    Fixing jade tests

--- packages/react-scripts/template/src/pages/root/Root.test.js
+++ packages/react-scripts/template/src/pages/root/Root.test.js
@@ -4,5 +4,5 @@ import Root from './Root';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Root />, div);
 });
